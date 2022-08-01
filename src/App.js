7166199d9ce94f2fba7d5581965a47d27c2b3e05
nashvador <nashvador@gmail.com>
2022-08-01 01:25:06 -0400

    Add call to API and add Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import UnderNavBar from "./components/underNavBar";
+import axios from "axios";
 
 function App() {
+  const [getApiInfo, setApiInfo] = useState([]);
+
+  const getApiCall = async () => {
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false"
+    );
+    setApiInfo(response.data);
+  };
+  useEffect(() => {
+    getApiCall();
+  }, []);
+
+  console.log(getApiInfo);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UnderNavBar />
+      {getApiInfo.map((coin) => (
+        <div> {coin.id}</div>
+      ))}
     </div>
   );
 }
---
+++ src/components/underNavBar.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const UnderNavBar = () => {
+  const [getGlobalInfo, setGlobalInfo] = useState({});
+
+  const getNavBarData = async () => {
+    const response = await axios.get("https://api.coingecko.com/api/v3/global");
+    setGlobalInfo(response.data.data);
+  };
+
+  useEffect(() => {
+    getNavBarData();
+  }, []);
+
+  console.log(getGlobalInfo);
+
+  const totalMarketValue = Object.values(getGlobalInfo.total_market_cap).reduce(
+    (accumulator, value) => {
+      return accumulator + value;
+    },
+    0
+  );
+
+  return (
+    <div>
+      Coins: {getGlobalInfo.active_cryptocurrencies}, Exchanges:{" "}
+      {getGlobalInfo.markets} Leader:{" "}
+      {Object.keys(getGlobalInfo.market_cap_percentage)[0]}{" "}
+      {Object.values(getGlobalInfo.market_cap_percentage)[0]}
+      Total market cap: {totalMarketValue} down by{" "}
+      {getGlobalInfo.market_cap_change_percentage_24h_usd}
+    </div>
+  );
+};
+
+export default UnderNavBar;
