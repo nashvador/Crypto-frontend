849dc0a887563e87d621f38b7a28fe6abdfffa27
nashvador <nashvador@gmail.com>
2022-09-20 11:43:51 -0400

    Change sign in page testing

--- crypto-frontend/src/__tests__/LogInPage.test.tsx
+++ crypto-frontend/src/__tests__/LogInPage.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import SignIn from "../components/UserAuthentication/LogInPage";
+import userEvent from "@testing-library/user-event";
 
 describe("<SignIn />", () => {
   let container: HTMLElement | null;
@@ -16,11 +17,6 @@ describe("<SignIn />", () => {
   });
 
   test("renders Sign in button", () => {
-    const element = screen.getByText("Sign in");
-    expect(element).toBeDefined();
-  });
-
-  test("renders Input Page", () => {
     const element = screen.getByText("Sign in");
     expect(element).toBeDefined();
   });
